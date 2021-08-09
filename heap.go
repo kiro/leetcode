90116cdb6ae58fc89860b09a355cb68d67327355
Kiril Minkov <kirilminkov@Kiril-MacBook-Pro-16.local>
2021-08-09 19:20:35 +0800

    Updating heap

--- heap.go
+++ heap.go
@@ -2,124 +2,96 @@ package leetcode
 
 import (
 	"container/heap"
-	"fmt"
 )
 
-type value int
-
-type Item struct {
-	v value
-	p int
-	i int
+type item struct {
+	item  Item
+	index int
 }
 
-func (i *Item) String() string {
-	return fmt.Sprintf("(v:%v p:%v i:%v)", i.v, i.p, i.i)
-}
-
-func item(v value, p int) *Item {
-	return &Item{v, p, 0}
-}
-
-func Min(l, r *Item) bool {
-	return l.p < r.p
-}
-
-func Max(l, r *Item) bool {
-	return l.p > r.p
-}
-
-type Heap struct {
+type heapImpl struct {
 	p               *pq
-	items           map[value]*Item
+	items           map[interface{}]*item
+	key             func(Item) interface{}
 	DisableIndexing bool
 }
 
-func NewHeap(compare func(l, r *Item) bool) *Heap {
-	return &Heap{
-		&pq{
-			[]*Item{},
-			compare,
-		},
-		make(map[value]*Item),
-		false,
-	}
+func (h *heapImpl) Push(i Item) {
+	it := &item{i, 0}
+	h.addItem(it)
+	heap.Push(h.p, it)
 }
 
-func (h *Heap) Push(i *Item) {
-	h.addItem(i)
-	heap.Push(h.p, i)
+func (h *heapImpl) Pop() Item {
+	res := heap.Pop(h.p).(*item)
+	h.removeItem(res.item)
+	return res.item
 }
 
-func (h *Heap) Pop() *Item {
-	res := heap.Pop(h.p).(*Item)
-	h.removeItem(res)
-	return res
-}
-
-func (h *Heap) Top() *Item {
+func (h *heapImpl) Top() Item {
 	if len(h.p.items) > 0 {
-		return h.p.items[0]
+		return h.p.items[0].item
 	}
 	return nil
 }
 
-func (h *Heap) Len() int {
+func (h *heapImpl) Len() int {
 	return len(h.p.items)
 }
 
-func (h *Heap) Update(item *Item, v value, p int) {
-	h.removeItem(item)
-	item.v = v
-	item.p = p
-	heap.Fix(h.p, item.i)
-	h.addItem(item)
+func (h *heapImpl) Update(o Item, n Item) {
+	old := h.items[h.key(o)]
+	new := &item{n, old.index}
+	h.removeItem(old.item)
+	h.p.items[old.index] = new
+	heap.Fix(h.p, old.index)
+	h.addItem(new)
 }
 
-func (h *Heap) Remove(item *Item) {
-	h.removeItem(item)
-	heap.Remove(h.p, item.i)
+func (h *heapImpl) Remove(i Item) {
+	index := h.items[h.key(i)].index
+	h.removeItem(i)
+	heap.Remove(h.p, index)
 }
 
-func (h *Heap) Get(v value) *Item {
-	return h.items[v]
+func (h *heapImpl) Get(key interface{}) Item {
+	return h.items[key].item
 }
 
-func (h *Heap) removeItem(item *Item) {
+func (h *heapImpl) removeItem(item Item) {
 	if !h.DisableIndexing {
-		delete(h.items, item.v)
+		delete(h.items, h.key(item))
 	}
 }
 
-func (h *Heap) addItem(item *Item) {
+func (h *heapImpl) addItem(item *item) {
 	if !h.DisableIndexing {
-		h.items[item.v] = item
+		h.items[h.key(item.item)] = item
 	}
 }
 
 // A PriorityQueue implements heap.Interface and holds Items.
 type pq struct {
-	items   []*Item
-	compare func(l, r *Item) bool
+	items []*item
 }
 
 func (pq pq) Len() int { return len(pq.items) }
 
 func (pq pq) Less(i, j int) bool {
 	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
-	return pq.compare(pq.items[i], pq.items[j])
+	return pq.items[i].item.Compare(pq.items[j].item)
 }
 
 func (pq pq) Swap(i, j int) {
 	pq.items[i], pq.items[j] = pq.items[j], pq.items[i]
-	pq.items[i].i = i
-	pq.items[j].i = j
+	pq.items[i].index = i
+	pq.items[j].index = j
 }
 
 func (pq *pq) Push(x interface{}) {
 	n := len(pq.items)
-	item := x.(*Item)
-	item.i = n
+	item := x.(*item)
+	item.index = n
 	pq.items = append(pq.items, item)
 }
 
@@ -127,8 +99,49 @@ func (pq *pq) Pop() interface{} {
 	old := pq.items
 	n := len(old)
 	item := old[n-1]
-	old[n-1] = nil // avoid memory leak
-	item.i = -1    // for safety
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
 	pq.items = old[0 : n-1]
 	return item
+}
+
+func NewHeapWithKey(getKey func(Item) interface{}) Heap {
+	return &heapImpl{
+		&pq{
+			[]*item{},
+		},
+		make(map[interface{}]*item),
+		getKey,
+		false,
+	}
+}
+
+func NewHeap() Heap {
+	return NewHeapWithKey(func(i Item) interface{} { return i })
+}
+
+type Item interface {
+	Compare(left Item) bool
+}
+
+type Heap interface {
+	Push(i Item)
+	Pop() Item
+	Top() Item
+	Len() int
+	Update(old Item, new Item)
+	Remove(item Item)
+	Get(key interface{}) Item
+}
+
+type MinInt int
+
+func (i MinInt) Compare(j Item) bool {
+	return i < j.(MinInt)
+}
+
+type MaxInt int
+
+func (i MaxInt) Compare(j Item) bool {
+	return i > j.(MaxInt)
 }
